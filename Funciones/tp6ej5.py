8a973acba8cd0a5f95d857fac9670b5b9c4a38ad
Fede Trujillo <fede4895@gmail.com>
2021-06-19 10:26:36 -0300

    creación de la función que descifra
    La función descifra un archivo de texto(.cesar) y retorna un archivo de tipo (.decode)
    
    Para descifrar se usa el cifrado del cesar (tp5ej8)

---
+++ Funciones/tp6ej5.py
@@ -0,0 +1,38 @@
+################
+# Fede Trujillo - @FNT138
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp5ej8 import descifrado_cesar
+
+
+def descifrar(texto, cifrado, rotacion):
+    lista = ""
+    with open(texto, encoding="utf-8") as arch:
+        with open(cifrado, "x") as cifr:
+            for linea in arch:
+                lista = lista + (linea)
+            re = reemplazar(lista)
+            descifrar = descifrado_cesar(re, rotacion)
+            cifr = cifr.write(descifrar)
+            return"FIN"
+                
+def reemplazar(cadena):
+    cadena = str(cadena)
+    cadena = str.rstrip(cadena)
+    return cadena                
+                
+            
+        
+def prueba():
+    txt = str(input("Nombre del archivo a descifrar  SIN EXTENCION:  "))
+    ext = ".cesar"
+    rotacion = int(input("Ingrese la rotacion del cifrado: "))
+    archivo = txt+ext
+    ext = ".decode"
+    descifrado = txt+ext
+    funcion = descifrar(archivo, descifrado, rotacion)
+    print(funcion)
+    
+if __name__ == "__main__":
+    prueba()
